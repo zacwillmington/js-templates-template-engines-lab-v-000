a59922759e2605cd321aa06f1ff3d7359d80ec81
Zac Willmington <zac.willmington@gmail.com>
2018-09-21 17:09:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function createPost() {
 
     let pageTemplate = document.getElementById("page-template").innerHTML;
     let postTemplate = document.getElementById("post-template").innerHTML;
-    let commentTemplate =
+    let commentTemplate = document.getElementById("comment-template").innerHTML;
 
     let templateFn = _.template(postTemplate);
 
